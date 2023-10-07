7d6d78a8152ef1456badb00c6b7c2ff50a6b1613
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-10-07 21:59:28 +0530

    Create comparator1bit.v

---
+++ comparator1bit.v
@@ -0,0 +1,21 @@
+//Verilog Code for 1 Bit Comparator
+module comparator1bit(A,B,AeB,AgB,AlB);
+
+input A,B;
+output reg AeB,AgB,AlB;
+
+always@(A,B)
+begin
+
+AeB=1'b0;AgB=1'b0;AlB=1'b0;
+
+case({A,B})
+2'b00:AeB<=1'b1;
+2'b01:AlB=1'b1;
+2'b10:AgB=1'b1;
+2'b11:AeB<=1'b1;
+endcase
+
+end
+
+endmodule
